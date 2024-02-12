d02b044820b025a623b896fe1484f36d771dd5ff
Alisher <aisroilov1029@gmail.com>
2024-02-12 23:09:01 +0500

    chart added

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
@@ -90,6 +91,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
+          Chart(expenses: _requiredExpenses),
           Expanded(child: mainContent),
         ],
       ),
